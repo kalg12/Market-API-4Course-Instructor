54a4c428e309da259cf9d4168ccb46fe4e2c4690
Kevin Luciano <luciano19940@hotmail.com>
2026-01-04 23:36:58 -0600

    Added: Sum endpoint that returns a number

---
+++ src/main/java/com/fulltech/market/GettingAnOperation.java
@@ -0,0 +1,16 @@
+package com.fulltech.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/get")
+
+
+public class GettingAnOperation {
+    @GetMapping("/sum")
+    public Number getSum(){
+        return 5+5;
+    }
+}
