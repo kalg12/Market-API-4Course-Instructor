8da5dfa6cea77eb596ccde5d5eb4fe0505b5d469
Kevin Luciano <luciano19940@hotmail.com>
2026-02-14 10:17:28 -0600

    Packages order implemented and JPA added

--- src/main/java/com/fulltech/market/web/controller/HelloWorldController.java
+++ src/main/java/com/fulltech/market/web/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.fulltech.market;
+package com.fulltech.market.web.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
