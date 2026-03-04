d6a8c4da8503373a2c4fe4b290afd679ff160114
Kevin Luciano <luciano19940@hotmail.com>
2026-03-04 11:47:12 -0600

    Dev properties for connection to DB

---
+++ src/main/java/com/fulltech/market/persistence/entity/Producto.java
@@ -0,0 +1,10 @@
+package com.fulltech.market.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCTOS")
+public class Producto {
+    
+}
