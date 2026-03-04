268454a59c498533bd3d3c762fd2dff838aa9c2b
Kevin Luciano <luciano19940@hotmail.com>
2026-03-04 13:01:18 -0600

    Product entity added

--- src/main/java/com/fulltech/market/persistence/entity/Producto.java
+++ src/main/java/com/fulltech/market/persistence/entity/Producto.java
@@ -1,10 +1,55 @@
 package com.fulltech.market.persistence.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "PRODUCTOS")
+@Entity //Defines the model as an entity that will be mapped to a database table
+@Table(name = "PRODUCTOS") // Specifies the name of the database table to which this entity will be mapped
 public class Producto {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    private Double precio;
+
+    private Integer cantidad;
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
 }
